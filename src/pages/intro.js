79acf6e679d8ebe21ced67ddd121b09f34cf6c0d
Ragzgit <ragavsrini10@gmail.com>
2023-03-09 17:07:52 -0500

    animation for intro

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -6,7 +6,7 @@ function Intro() {
     <div id="lead-content">
         <h1>Ragavan Srinivasan</h1>
         <h2>Software Engineer</h2>
-        <a href="https://drive.google.com/file/d/1YsJZK2TyaI7UnXn-wjyBWOsFLsKMgV75/view?usp=share_link" className="btn-rounded-white">Download Resume</a>
+        <a style={{textDecoration: "none"}}href="https://drive.google.com/file/d/1YsJZK2TyaI7UnXn-wjyBWOsFLsKMgV75/view?usp=share_link" target="_blank" className="btn-rounded-white">Download Resume</a>
     </div>
 
     <div id="lead-overlay"></div>
